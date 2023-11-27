a36d4497617bf42cb8ca6701d215c8f25cd141bb
Karlyn Sanders <karlynwright@gmail.com>
2023-11-27 13:04:19 -0700

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by Karlyn Sanders on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Karlyn Sanders on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
